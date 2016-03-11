3a6138fcaf58ab5bc670674ae94a0e64003963c1
Cameron White <cameron.white@janrain.com>
2016-03-10 16:52:07 -0800

    Expanded bluekai_tsv module tests

--- tests/test_bluekai_tsv.py
+++ tests/test_bluekai_tsv.py
@@ -9,6 +9,18 @@ class bluekai_test(TestCase):
     expected = "a-b-c\tkey2=value2|key1=value1\n"
     self.assertEqual(actual, expected)
 
+  def test_fromRecordsIterator(self):
+    record1 = { "uuid": "a-b-c", "key1": "value1" }
+    record2 = { "uuid": "x-y-z", "key2": "value2" }
+    records = [record1, record2]
+    records_iterator = fromRecordsIterator(records)
+    expected1 = "a-b-c\tkey1=value1\n"
+    expected2 = "x-y-z\tkey2=value2\n"
+    self.assertEqual(records_iterator.next(), expected1)
+    self.assertEqual(records_iterator.next(), expected2)
+    with self.assertRaises(StopIteration):
+        records_iterator.next()
+
   def test_fromRecords(self):
     record1 = { "uuid": "a-b-c", "key1": "value1" }
     record2 = { "uuid": "x-y-z", "key2": "value2" }
