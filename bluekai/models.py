723ab248abfd6b74305d1d933229a65d20f953f8
Cameron White <cameron.white@janrain.com>
2016-03-10 16:51:19 -0800

    Updated models module + tests

--- bluekai/models.py
+++ bluekai/models.py
@@ -1,6 +1,7 @@
 import pynamodb.attributes
 import pynamodb.models
 from pynamodb.exceptions import DoesNotExist
+from datetime import datetime
 from .config import get_config
 from .date_utils import fromModelDateTime
 from .date_utils import toModelDateTime
@@ -16,17 +17,75 @@ class JobModel(pynamodb.models.Model):
         host = config['AWS_DYNAMODB_URL']
 
     app_id = pynamodb.attributes.UnicodeAttribute(hash_key=True)
-    _last_updated = pynamodb.attributes.NumberAttribute()
+    _last_updated = pynamodb.attributes.NumberAttribute(null=True)
+    _started = pynamodb.attributes.NumberAttribute(null=True)
+    _ended = pynamodb.attributes.NumberAttribute(null=True)
+    _running = pynamodb.attributes.BooleanAttribute(null=True)
+    _error = pynamodb.attributes.UnicodeAttribute(null=True)
+
+    def start(self):
+        """Start the job. Can only be started if not currently running.
+
+        Args:
+            filename: name of file
+
+        Returns:
+            bool (whether start succeeded)
+        """
+        self._started = toModelDateTime(datetime.now())
+        self._ended = None
+        self._running = True
+        self._error = None
+        try:
+            # only start job if not currently running
+            self.save(_running__null=True)
+        except pynamodb.exceptions.PutError as ex:
+            if 'ConditionalCheckFailedException' in str(ex):
+                return False
+            raise
+        return True
+
+    def stop(self):
+        """Stop the job."""
+        self._ended = toModelDateTime(datetime.now())
+        self._running = None
 
     @property
     def lastUpdated(self):
-        last_updated = self._last_updated or 0
-        return fromModelDateTime(last_updated)
+        last_updated = self._last_updated
+        if last_updated is not None:
+            return fromModelDateTime(last_updated)
+        else:
+            return None
 
     @lastUpdated.setter
     def lastUpdated(self, last_updated):
-        self._last_updated = last_updated
+        self._last_updated = last_updated and toModelDateTime(last_updated)
         self.save()
+
+    @property
+    def started(self):
+        started = self._started
+        if started is not None:
+            return fromModelDateTime(started)
+        else:
+            return None
+
+    @property
+    def ended(self):
+        ended = self._ended
+        if ended is not None:
+            return fromModelDateTime(ended)
+        else:
+            return None
+
+    @property
+    def running(self):
+        return self._running
+
+    @property
+    def error(self):
+        return self._error
 
     @classmethod
     def get(cls, config):
@@ -41,4 +100,3 @@ class JobModel(pynamodb.models.Model):
         janrain_uri = config.get('JANRAIN_URI')
         janrain_schema_name = config.get('JANRAIN_SCHEMA_NAME')
         return "{}:{}".format(janrain_uri, janrain_schema_name)
-
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,14 @@
 from unittest import TestCase
 from mock import patch
 from mock import Mock
+from datetime import datetime
+import pynamodb.models
+from pynamodb.exceptions import DoesNotExist
+from pynamodb.exceptions import PutError
 from bluekai.models import *
+from freezegun import freeze_time
+
+frozen_datetime="2016-01-01"
 
 class models_test(TestCase):
 
@@ -16,3 +23,165 @@ class models_test(TestCase):
         actual = JobModel.appId(self.config)
         expected = "{}:{}".format(self.config['JANRAIN_URI'], self.config['JANRAIN_SCHEMA_NAME'])
         self.assertEqual(actual, expected)
+
+    @patch.object(JobModel, 'save')
+    @freeze_time(frozen_datetime)
+    def test_start(self, save_mock):
+
+
+        job = JobModel()
+        result = job.start()
+
+        self.assertEqual(job.started, datetime.strptime(frozen_datetime, '%Y-%m-%d'))
+        self.assertIsNone(job.ended)
+        self.assertTrue(job.running)
+        self.assertIsNone(job.error)
+        save_mock.assert_called_once_with(_running__null=True)
+        self.assertTrue(result)
+
+    @patch.object(JobModel, 'save')
+    @freeze_time(frozen_datetime)
+    def test_start_when_job_is_alread_running(self, save_mock):
+
+        save_mock.side_effect = PutError('ConditionalCheckFailedException')
+
+        job = JobModel()
+        result = job.start()
+
+        self.assertEqual(job.started, datetime.strptime(frozen_datetime, '%Y-%m-%d'))
+        self.assertIsNone(job.ended)
+        self.assertTrue(job.running)
+        self.assertIsNone(job.error)
+        save_mock.assert_called_once_with(_running__null=True)
+        self.assertFalse(result)
+
+    @patch.object(JobModel, 'save')
+    @freeze_time(frozen_datetime)
+    def test_start_error(self, save_mock):
+
+        save_mock.side_effect = PutError()
+
+        job = JobModel()
+        with self.assertRaises(PutError):
+            result = job.start()
+
+        self.assertEqual(job.started, datetime.strptime(frozen_datetime, '%Y-%m-%d'))
+        self.assertIsNone(job.ended)
+        self.assertTrue(job.running)
+        self.assertIsNone(job.error)
+        save_mock.assert_called_once_with(_running__null=True)
+
+    def test_lastUpdated_getter_when_none(self):
+
+        job = JobModel()
+        result = job.lastUpdated
+
+        self.assertIsNone(result)
+
+    def test_lastUpdated_getter_when_not_none(self):
+
+        job = JobModel()
+        job._last_updated = 0
+        result = job.lastUpdated
+
+        self.assertEqual(result, datetime.utcfromtimestamp(0))
+
+    @patch.object(JobModel, 'save')
+    def test_lastUpdated_setter_when_none(self, save_mock):
+
+        job = JobModel()
+        job.lastUpdated = None
+
+        self.assertIsNone(job._last_updated)
+        save_mock.assert_called_with()
+
+    @patch.object(JobModel, 'save')
+    def test_lastUpdated_setter_when_not_none(self, save_mock):
+
+        job = JobModel()
+        job.lastUpdated = datetime.utcfromtimestamp(0)
+
+        self.assertEqual(job._last_updated, 0)
+        save_mock.assert_called_with()
+
+    def test_started_getter_when_none(self):
+
+        job = JobModel()
+        result = job.started
+
+        self.assertIsNone(result)
+
+    def test_started_getter_when_not_none(self):
+
+        job = JobModel()
+        job._started = 0
+        result = job.started
+
+        self.assertEqual(result, datetime.utcfromtimestamp(0))
+
+    def test_ended_getter_when_none(self):
+
+        job = JobModel()
+        result = job.started
+
+        self.assertIsNone(result)
+
+    def test_ended_getter_when_not_none(self):
+
+        job = JobModel()
+        job._started = 0
+        result = job.started
+
+        self.assertEqual(result, datetime.utcfromtimestamp(0))
+
+    def test_running(self):
+
+        job = JobModel()
+        job._running = True
+        result = job.running
+
+        self.assertTrue(result)
+
+    def test_running(self):
+
+        job = JobModel()
+        job._error = "test error message"
+        result = job.error
+
+        self.assertEqual(result, "test error message")
+
+    @freeze_time(frozen_datetime)
+    def test_stop(self):
+
+        job = JobModel()
+        job.stop()
+
+        self.assertEqual(job.ended, datetime.strptime(frozen_datetime, '%Y-%m-%d'))
+        self.assertIsNone(job.running)
+
+    @patch.object(JobModel, 'appId')
+    @patch.object(JobModel, '__init__')
+    @patch.object(pynamodb.models.Model, 'get')
+    def test_get_when_item_exists(self, model_get_mock, init_mock, appId_mock):
+        app_id = "test appId"
+        init_mock.return_value = None
+        appId_mock.return_value = app_id
+
+        JobModel.get(self.config)
+
+        model_get_mock.assert_called_once_with(app_id)
+        init_mock.assert_not_called()
+
+    @patch.object(JobModel, 'appId')
+    @patch.object(JobModel, '__init__')
+    @patch.object(pynamodb.models.Model, 'get')
+    def test_get_when_item_exists(self, model_get_mock, init_mock, appId_mock):
+        app_id = "test appId"
+
+        init_mock.return_value = None
+        appId_mock.return_value = app_id
+        model_get_mock.side_effect = DoesNotExist()
+
+        JobModel.get(self.config)
+        model_get_mock.assert_called_once_with(app_id)
+        init_mock.assert_called_once_with(app_id)
