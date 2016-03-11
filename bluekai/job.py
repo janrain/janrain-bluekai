91bb9442836acc7d8514c4957442062120068973
Cameron White <cameron.white@janrain.com>
2016-03-10 17:30:09 -0800

    updated job and resources to use threading

--- bluekai/job.py
+++ bluekai/job.py
@@ -1,33 +1,45 @@
 from .date_utils import fromRecordDateTime
 from .date_utils import toRecordDateTime
-from .date_utils import fromModelDateTime
-from .date_utils import toModelDateTime
 
 def run(JobModel, writter, config, logger, records_iterator, converter):
 
-    job = JobModel.get(config)
-    last_updated = job.lastUpdated
-
     logger.info("start")
 
-    with writter as fp:
+    error = None
 
-        record_num = 0
-        for record_num, record in enumerate(records_iterator, start=1):
-
-            record_last_updated = fromRecordDateTime(record['lastUpdated'])
-            if record_last_updated > last_updated:
-                last_updated = fromRecordDateTime(record['lastUpdated'])
-                job.lastupdated = toModelDateTime(last_updated)
-
-            row = converter(record)
-
-            # write to remote file
-            fp.write(row)
-
-            if record_num % config['JANRAIN_BATCH_SIZE'] == 0:
-                logger.debug("wrote record {}".format(record_num))
-
-        logger.info("exported {} records".format(record_num))
+    try:
+        job = JobModel.get(config)
+        last_updated = job.lastUpdated
 
 
+        with writter as fp:
+
+            record_num = 0
+            for record_num, record in enumerate(records_iterator, start=1):
+
+                record_last_updated = fromRecordDateTime(record['lastUpdated'])
+                if not last_updated or record_last_updated > last_updated:
+                    last_updated = fromRecordDateTime(record['lastUpdated'])
+                    job.lastupdated = last_updated
+
+                row = converter(record)
+
+                fp.write(row)
+
+                if record_num % config['JANRAIN_BATCH_SIZE'] == 0:
+                    logger.debug("wrote record {}".format(record_num))
+
+            logger.info("exported {} records".format(record_num))
+
+    except Exception as ex:
+
+        err = str(ex)
+        logger.exception(ex)
+
+    finally:
+        if err:
+            job.error = err
+            logger.error(err)
+        # update db regardless
+        job.stop()
+        logger.info("end ({} seconds)".format(job.ended - job.started))
--- bluekai/resources.py
+++ bluekai/resources.py
@@ -1,13 +1,15 @@
 import flask
 import logging
+import json
 import janrain_datalib
 import janrain_datalib.exceptions
 import janrain_datalib.utils
 import bluekai_tsv
-from . import job
+from .job import run as jobRunner
 from .models import JobModel
 from .sftpproxy import SftpProxy
 from .records import recordsNewerThan
+from .date_utils import toRecordDateTime
 
 
 def export():
@@ -23,15 +25,25 @@ def export():
         config['JANRAIN_CLIENT_ID'],
         config['JANRAIN_CLIENT_SECRET'])
 
-    jobItem = JobModel.get(config)
-    last_updated = jobItem.lastUpdated
+    job = JobModel.get(config)
+    last_updated = job.lastUpdated
 
     new_records_iterator = recordsNewerThan(capture_app, config, last_updated)
 
     sftp = SftpProxy(config, logger)
     writter = sftp.file(config['REMOTE_FILE'], mode='w', bufsize=config['SFTP_BUFFER_SIZE'])
 
-    flask.current_app.threadexecutor.submit(
-        job.run, JobModel, writter, config, logger, new_records_iterator, bluekai_tsv.fromRecord)
+    if job.start():
+        flask.current_app.threadexecutor.submit(
+            jobRunner, JobModel, writter, config, logger,
+            new_records_iterator, bluekai_tsv.fromRecord)
+    else:
+        logger.warning("export job already running")
 
-    return "ok", 200
+    return json.dumps({
+            "started": job.started and toRecordDateTime(job.started),
+            "ended": job.ended and toRecordDateTime(job.ended),
+            "running": job.running,
+            "lastUpdated": job.lastUpdated and toRecordDateTime(job.lastUpdated),
+            "error": job.error,
+        })
