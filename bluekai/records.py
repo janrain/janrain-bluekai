ad5f79590628a7c458c450e0cc18658be4daa4ae
Cameron White <cameron.white@janrain.com>
2016-04-01 13:44:01 -0700

    DIP-186 Added attributes to /entity.find call

--- bluekai/records.py
+++ bluekai/records.py
@@ -5,12 +5,15 @@ def recordsNewerThan(capture_app, config, date=None):
 
     capture_schema = capture_app.get_schema(config['JANRAIN_SCHEMA_NAME'])
 
+    attributes = set(config['JANRAIN_ATTRIBUTE_KEYS']) | set(('uuid', 'lastUpdated'))
+
     if not date:
       date = datetime.utcfromtimestamp(0)
 
     records_iterator = capture_schema.records.iterator(
         filtering="lastUpdated > '{}'".format(toRecordDateTime(date)),
-        batch_size=config['JANRAIN_BATCH_SIZE']
+        batch_size=config['JANRAIN_BATCH_SIZE'],
+        attributes=sorted(attributes)
     )
 
     return records_iterator
