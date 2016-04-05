f7e75ea27b4ea96f9eb00e7dcbc54ea0dd4b47c4
Cameron White <cameron.white@janrain.com>
2016-04-05 15:49:23 -0700

    DIP-164 improved tests + fixes

--- bluekai/bluekai_tsv.py
+++ bluekai/bluekai_tsv.py
@@ -17,50 +17,47 @@ def fromRecord(record, paths=None):
         except TypeError:
             return path
 
-    def mapValue(value):
-        if isinstance(value, (dict, list)):
-            raise TypeError("Plural, Object and JSON value types are not supported")
-        elif value is None:
-            return ""
-        else:
-            return value
-
     # Compile (path, regex) for each path
     regex_mappings = [
         (path, re.compile(path.replace('.', r'(?:\.|\.\d+\.)')))
         for path in (paths or [])
     ]
 
-    walked_record = walk(record)
+    try:
 
-    # (path, walked_path, value) of each walked_record that matches a
-    # regex and add path from regex_mapping.
-    walked_record = (
-        (path, walked_path, value)
-        for (walked_path, value) in walked_record
-        for (path, regex) in regex_mappings
-        if regex.fullmatch(walked_path)
-    )
+        walked_record = walk(record)
 
-    # Group by path
-    groups = groupby(walked_record, key=lambda each: each[0])
+        # (path, walked_path, value) of each walked_record that matches a
+        # regex and add path from regex_mapping.
+        walked_record = (
+            (path, walked_path, value)
+            for (walked_path, value) in walked_record
+            for (path, regex) in regex_mappings
+            if regex.fullmatch(walked_path)
+        )
 
-    # Join groups values with ','.
-    attributes = (
-        (path, str.join(',', [ value for (_, _, value) in group if value is not None ]))
-        for (path, group) in groups
-    )
+        # Group by path
+        groups = groupby(walked_record, key=lambda each: each[0])
 
-    # Join attributes keys and values with "=".
-    attributes = (
-       "{}={}".format(mapPath(path), mapValue(value))
-       for (path, value) in attributes
-    )
+        # Join groups values with ','.
+        attributes = (
+            (path, str.join(',', [ value for (_, _, value) in group if value is not None ]))
+            for (path, group) in groups
+        )
 
-    # Join attributes with '|'.
-    attributes = str.join('|', attributes)
+        # Join attributes keys and values with "=".
+        attributes = (
+           "{}={}".format(mapPath(path), value)
+           for (path, value) in attributes
+        )
 
-    # Create row by joining uuid and attributes.
-    row = "{}\t{}\n".format(record['uuid'], attributes)
+        # Join attributes with '|'.
+        attributes = str.join('|', attributes)
+
+        # Create row by joining uuid and attributes.
+        row = "{}\t{}\n".format(record['uuid'], attributes)
+
+    except TypeError:
+        raise TypeError("Plural, Object and JSON value types are not supported")
 
     return row
